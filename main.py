2b9d2f20e96b114032c2e6c1fe0f6b604a2ff728
3xp051t10n <brief@disroot.org>
2025-09-26 22:34:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-async def read_root():
-    return {"message": "THIS IS GET"}
-
-@app.post("/")
-async def write_root():
-    return {"message": "THIS NOT GET"}
+fastapi
+uvicorn[standard]
